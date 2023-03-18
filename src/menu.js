4f5a24289ea694c02919f4b32320c8558c638b72
LukaNikcevic77 <nikcevic.luka77@gmail.com>
2023-03-18 03:59:34 +0100

    tEST

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,16 @@
 export {StyleMenu};
-
-
+import Vector1 from '../design_photos_and_restourant_images/Vector1.svg';
+import Vector2 from '/design_photos_and_restourant_images/Vector2.svg';
+import Menu from '/design_photos_and_restourant_images/Menu.jpeg';
 const soup = document.createElement('h1');
 const tea = document.createElement('p');
-const image = document.createElement('img');
-const line1 = document.createElement('img');
-const line2 = document.createElement('img');
+const image = new Image();
+const line1 = new Image();
+const line2 = new Image();
 function StyleMenu(ambis) {
-  line1.setAttribute('src', '/design photos and restourant images/Vector 1.svg');
+  line1.src = Vector1;
   line1.setAttribute('id', 'Line-1');
-  line2.setAttribute('src', '/design photos and restourant images/Vector 2.svg');
+  line2.src = Vector2;
   line2.setAttribute('id', 'Line-2');
 
   soup.classList.add('Soup');
@@ -17,7 +18,7 @@ function StyleMenu(ambis) {
   tea.classList.add('Tea');
   tea.textContent = 'Tea 5$';
   image.classList.add('img_2');
-  image.setAttribute('src', '/design photos and restourant images/Menu.jpeg');
+  image.src = Menu;
 
 
   AppendMenu(ambis);
